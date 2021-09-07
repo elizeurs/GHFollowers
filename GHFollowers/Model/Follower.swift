5a9067b4dbf3ba3d8e8f99730185e9b12016dd94
elizeurs <13ders@gmail.com>
2021-09-07 11:57:22 -0300

    Model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Elizeu RS on 07/09/21.
+//
+
+import Foundation
+
+struct Follower: Codable {
+  var login: String
+  var avatarUrl: String
+}
